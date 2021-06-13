5997469c429faa84677ae7dac6bde6c373967228
NOS-DONE <trombonesterenko@gmail.com>
2021-06-13 14:38:18 +0200

    methods

---
+++ src/js/fetch-events.js
@@ -0,0 +1,57 @@
+
+
+/**
+ * Значення запиту по ключовому слову: екземпляр_класу.apiQuery = '...'
+ * Значення запиту по ключовому країні: екземпляр_класу.apiCountry = '...'
+ * Запит: екземпляр_класу.fetchEvents()
+ * Кожний успішний запит з такими самими параметрами завантажує наступну сторінку з 20 елементами 
+ * скинути сторінку до першої: екземпляр_класу.resetPage()
+ */
+
+
+export default class EventApiService {
+    constructor() {
+        this.searchEventByKeywordQuery = '';
+        this.searchCountryQuery = '';
+        this.page = 0;
+
+    }
+
+    fetchEvents() {
+        console.log(this);
+        const CONSUMER_KEY = '21thx8JLVbOdavwqxpr1UpomSd3AvkEA';
+        const url = `https://app.ticketmaster.com/discovery/v2/events.json?page=${this.page}&size=20&keyword=${this.searchEventByKeywordQuery}&countryCode=${this.searchCountryQuery}&apikey=${CONSUMER_KEY}`;
+        
+
+        fetch(url)
+            .then(responce => responce.json())
+            .then(data => {
+                console.log(data);
+                this.incrementPage()
+            })
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+    resetPage() {
+        this.page = 0;
+    }
+
+    set apiQuery(newApiQuery) {
+        this.searchEventByKeywordQuery = newApiQuery;
+    }
+
+    get apiQuery() {
+        return this.searchEventByKeywordQuery;
+    } 
+
+    set apiCountry(newApiCountry) {
+        this.searchCountryQuery = newApiCountry;
+    }
+
+    get apiQuery() {
+        return this.searchCountryQuery;
+    } 
+}
+
