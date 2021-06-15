d8ee2097b4523a910fc7bb302a2a3b111bcbbf61
Oleh-N-Lunin <67375261+Oleh-N-Lunin@users.noreply.github.com>
2021-06-15 20:32:51 +0300

    last commit

--- src/js/pages.js
+++ src/js/pages.js
@@ -14,15 +14,13 @@ function testApi(e) {
         .then(data => console.log(data))
         
 }
-
+console.log(arr)
 testApi(formRef);
 
-const testDate = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
 $('.pagenumbers').pagination({
-    dataSource: testDate,
+    dataSource: arr,
     callback: function (data, pagination) {
-        var html = template(testDate);
+        var html = template(arr);
         $('#data-container').html(html);
     }
 })
