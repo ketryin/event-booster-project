fcb545c409d99b2a4a202b6dc2f2aa35aa66de0c
APilganchuk <opilganchuk@gmail.com>
2021-06-15 14:05:06 +0300

    disables scrolling when modal is open

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,8 +1,9 @@
 const refs = {
   eventCards: document.querySelector('.events-list'),
   modalWindow: document.querySelector('#modal-card'),
-  modalBtnClose: document.querySelector('.modal-box .modal__btn'),
-  backdrop: document.querySelector('.backdrop'),
+  modalBtnClose: document.querySelector('[data-modal-window-close]'),
+  backdrop: document.querySelector('[data-modal-backdrop]'),
+  body: document.querySelector('body'),
 };
 
 refs.eventCards.addEventListener('click', onEventCardClick);
@@ -12,13 +13,15 @@ refs.backdrop.addEventListener('click', onClickBackdrop);
 
 function onCLicklBtnClose() {
   refs.modalWindow.classList.toggle('is--hidden');
+  refs.body.classList.toggle('modal-open');
 }
 
 function onClickBackdrop(e) {
-  if (!e.target.classList.contains('backdrop')) {
+  if (!e.target.classList.contains('backdrop__modal')) {
     return;
   }
   refs.backdrop.classList.toggle('is--hidden');
+  refs.body.classList.toggle('modal-open');
 }
 
 function onEventCardClick(e) {
@@ -27,4 +30,5 @@ function onEventCardClick(e) {
     return;
   }
   refs.modalWindow.classList.toggle('is--hidden');
+  refs.body.classList.toggle('modal-open')
 }
