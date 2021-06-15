d5425925c78ac75e007793004b39e3962c37bf73
LidiaStrelets <0630610171li@gmail.com>
2021-06-15 12:11:12 +0300

    loads initial events

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import './sass/main.scss';
 import countries from './js/countries.js';
-import modal from './js/modal.js'
+import modal from './js/modal.js';
 import populateSelect from './js/select-populating.js';
-import modalTeam from './js/modal-team.js'
+import modalTeam from './js/modal-team.js';
 import handleFormChange from './js/listen-form-events.js';
-
+import initialPagePopulate from './js/initial-fetch';
 
 const selectRef = document.querySelector('#country-select');
 const formRef = document.querySelector('.events__search-form');
 const eventsListRef = document.querySelector('.js-list');
 const inputRef = document.querySelector('#query-input');
 
+initialPagePopulate(eventsListRef);
 populateSelect(countries, selectRef);
 handleFormChange(formRef, eventsListRef, selectRef, inputRef);
-// animateSpinner(spinnerRef);
