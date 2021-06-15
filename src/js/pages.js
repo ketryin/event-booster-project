2c53d9d9198d8a9fb46f6cf582db88d99e4502a7
Oleh-N-Lunin <67375261+Oleh-N-Lunin@users.noreply.github.com>
2021-06-15 20:02:35 +0300

    Заканчивает работу с API

--- src/js/pages.js
+++ src/js/pages.js
@@ -2,18 +2,19 @@ import ApiService from './fetch-events.js'
 
 const paginationApi = new ApiService();
 const formRef = document.querySelector('.events__search-form');
-let arr = []
+let arr;
 function testApi(e) {
     if (e.target.nodeName === 'INPUT') {
         paginationApi.apiQuery = e.target.value;
     }
 
-    paginationApi.fetchEvents()
-        .then(data => arr = data._embedded)
+    paginationApi
+        .fetchEvents()
+        .then(data => arr = data._embedded.events)
+        .then(data => console.log(data))
         
 }
 
-console.log(arr)
 testApi(formRef);
 
 const testDate = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
