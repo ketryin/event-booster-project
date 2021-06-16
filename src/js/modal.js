731a1f055631211cf1e5d88e6e497f468c8edafa
APilganchuk <opilganchuk@gmail.com>
2021-06-16 11:55:35 +0300

    changes

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,8 @@
+import EventApiService from './fetch-events.js';
+import modalTemplate from '../templates/modal-card-details.hbs';
+
+const api = new EventApiService();
+
 const refs = {
   eventCards: document.querySelector('.events-list'),
   modalWindow: document.querySelector('#modal-card'),
@@ -29,6 +34,22 @@ function onEventCardClick(e) {
   if (!currentCard.closest('.events-list__item')) {
     return;
   }
-  refs.modalWindow.classList.toggle('is--hidden');
-  refs.body.classList.toggle('modal-open')
+
+  const eventSingleCard = currentCard.closest('.events-list__item');
+  console.log(eventSingleCard);
+
+  if (e.target.nodeName === 'IMG' || e.target.nodeName === 'SPAN') {
+    api
+      .fetchModalDetails(eventSingleCard.id, eventSingleCard.dataset.type)
+      .then(data => {
+        console.log(data);
+        // console.log(refs.modalWindow);
+        // console.log(modalTemplate(data));
+
+        refs.modalWindow.innerHTML = modalTemplate(data);
+        refs.modalWindow.classList.toggle('is--hidden');
+        refs.body.classList.toggle('modal-open');
+      })
+      .catch(error => console.log(error));
+  }
 }
