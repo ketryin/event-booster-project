6d2e349994dcfdbe193e5b19e47cf8719e9e23f8
Kateryna Kraieva <70651413+ketryin@users.noreply.github.com>
2021-06-17 09:39:40 +0300

    Revert "add scroll smoth"
    This reverts commit 1946889ec302552a103aeec6816161d27de91ca6.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import modal from './js/modal.js';
 import populateSelect from './js/select-populating.js';
 import modalTeam from './js/modal-team.js';
 import handleFormChange from './js/listen-form-events.js';
-import initialPagePopulate from './js/initial-fetch.js';
-import arrowUp from './js/arrow-up.js';
+import initialPagePopulate from './js/initial-fetch';
 
 const selectRef = document.querySelector('#country-select');
 const formRef = document.querySelector('.events__search-form');
