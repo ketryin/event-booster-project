7381001b9fc3e76b7a196d062758b142d3948d45
LidiaStrelets <0630610171li@gmail.com>
2021-06-18 19:14:17 +0300

    input should work

--- src/js/listen-form-events.js
+++ src/js/listen-form-events.js
@@ -13,7 +13,7 @@ export default function handleFormChange(form, list, select, input) {
     animateLoader();
     event.preventDefault();
     api.apiQuery = input.value;
-
+    console.log(api);
     handleFetch();
   }
 
@@ -26,6 +26,7 @@ export default function handleFormChange(form, list, select, input) {
 
   function handleFetch() {
     api.resetPage();
+    console.log('in handle', api);
     api
       .fetchEvents()
       .then(data => {
