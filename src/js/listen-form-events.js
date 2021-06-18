148736b99f4570970c93146211757a184beb8b87
LidiaStrelets <0630610171li@gmail.com>
2021-06-18 18:20:57 +0300

    my changes

--- src/js/listen-form-events.js
+++ src/js/listen-form-events.js
@@ -1,66 +1,66 @@
 import ApiService from './fetch-events.js';
 import cardTpl from './../templates/event-card.hbs';
-import debounce from 'lodash.debounce';
 import animateLoader from './show-loader';
 import removeLoader from './remove-loader';
 
-export default function handleFormChange(form, list, select, input, customSelect) {
+export default function handleFormChange(form, list, select, input) {
   const api = new ApiService();
 
-  input.addEventListener('input', debounce(handleInput, 500));
-
   select.addEventListener('change', handleSelect);
+  form.addEventListener('submit', handleFormChange);
 
-  // form.addEventListener('change', handleFormChange);
   function handleFormChange(event) {
-    event.preventDefault();
-    if (event.target.nodeName === 'INPUT') {
-      api.apiQuery = event.target.value;
-    }
-    if (event.target.nodeName === 'SELECT') {
-      api.apiCountry = event.target.value;
-    }
-    populatePage();
-  }
-
-  function handleInput(event) {
-    api.resetPage();
     animateLoader();
-    if (event.target.value === '') {
-      removeLoader();
-      return;
-    }
-    api.apiQuery = event.target.value;
-    populatePage();
+    event.preventDefault();
+    api.apiQuery = input.value;
+
+    handleFetch();
   }
 
   function handleSelect() {
-    api.resetPage();
     animateLoader();
-    api.apiCountry = select.options[select.selectedIndex].value;
-    populatePage();
+    api.apiCountry = select.value;
+
+    handleFetch();
   }
-  
-   function populatePage() {
+
+  function handleFetch() {
+    api.resetPage();
+    api
+      .fetchEvents()
+      .then(data => {
+        list.innerHTML = cardTpl(data._embedded.events);
+      })
+      .catch(error => {
+        alert(error);
+        list.innerHTML = '';
+      })
+      .finally(removeLoader);
+  }
+
+  function populatePage() {
+    animateLoader();
+    api.searchCountryQuery = 'DK';
+
     $('#pagenumbers').pagination({
       ajax: function (options, refresh, $target) {
-        api.page = options.current-1;
-        api.fetchEvents().then(function (data) {
-          refresh({
-            total: data.page.totalElements,
-            length: data.page.size
-          });
-          list.innerHTML = cardTpl(data._embedded.events);
-        }).catch(alert)
+        api.page = options.current - 1;
+        api
+          .fetchEvents()
+          .then(function (data) {
+            refresh({
+              total: data.page.totalElements,
+              length: data.page.size,
+            });
+            list.innerHTML = cardTpl(data._embedded.events);
+          })
+          .catch(alert)
           .finally(() => {
             removeLoader();
-            form.reset();
-            console.log(customSelect.setChoiceByValue);
-            customSelect.setChoiceByValue('');
           });
       },
     });
-
   }
-  populatePage()
+
+  populatePage();
 }
