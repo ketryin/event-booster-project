927b6eb22d52413c26a34f26c21e0deff57bf783
IhorWenher <73756717+IhorWenher@users.noreply.github.com>
2021-06-21 22:48:38 +0300

    add location link

--- src/js/modal.js
+++ src/js/modal.js
@@ -6,85 +6,85 @@ import filterBiggerImage from './filter-lagest-image.js';
 const api = new EventApiService();
 
 const refs = {
-  eventCards: document.querySelector('.events-list'),
-  modalWindow: document.querySelector('#modal-card'),
-  modalBtnClose: document.querySelector('[data-modal-window-close]'),
-  backdrop: document.querySelector('[data-modal-backdrop]'),
-  body: document.querySelector('body'),
+    eventCards: document.querySelector('.events-list'),
+    modalWindow: document.querySelector('#modal-card'),
+    modalBtnClose: document.querySelector('[data-modal-window-close]'),
+    backdrop: document.querySelector('[data-modal-backdrop]'),
+    body: document.querySelector('body'),
 };
 
 refs.eventCards.addEventListener('click', onEventCardClick);
 refs.backdrop.addEventListener('click', onClickBackdrop);
 
 export function onClickBackdrop(e) {
-  if (!e.target.classList.contains('backdrop__modal')) {
-    return;
-  }
-  // refs.modalWindow.innerHTML = '';
-  refs.backdrop.classList.toggle('is--hidden');
-  refs.body.classList.toggle('modal-open');
+    if (!e.target.classList.contains('backdrop__modal')) {
+        return;
+    }
+    // refs.modalWindow.innerHTML = '';
+    refs.backdrop.classList.toggle('is--hidden');
+    refs.body.classList.toggle('modal-open');
 }
 
 function onCLickBtnClose() {
-  const btnRef = document.querySelector('[data-modal-window-close]');
-  // refs.modalWindow.innerHTML = '';
+    const btnRef = document.querySelector('[data-modal-window-close]');
+    // refs.modalWindow.innerHTML = '';
 
-  btnRef.addEventListener('click', () => {
-    refs.modalWindow.classList.toggle('is--hidden');
-    refs.body.classList.toggle('modal-open');
-  });
+    btnRef.addEventListener('click', () => {
+        refs.modalWindow.classList.toggle('is--hidden');
+        refs.body.classList.toggle('modal-open');
+    });
 }
 
 function onEventCardClick(e) {
-  const currentCard = e.target;
-  if (!currentCard.closest('.events-list__item')) {
-    return;
-  }
+    const currentCard = e.target;
+    if (!currentCard.closest('.events-list__item')) {
+        return;
+    }
 
-  const eventSingleCard = currentCard.closest('.events-list__item');
-  refs.modalWindow.classList.toggle('is--hidden');
-  refs.body.classList.toggle('modal-open');
+    const eventSingleCard = currentCard.closest('.events-list__item');
+    refs.modalWindow.classList.toggle('is--hidden');
+    refs.body.classList.toggle('modal-open');
 
-  if (e.target.nodeName === 'IMG' || e.target.nodeName === 'SPAN' || e.target.nodeName === 'H2' || e.target.nodeName === 'P' || e.target.nodeName === 'LI') {
-    api
-      .fetchModalDetails(eventSingleCard.id, eventSingleCard.dataset.type)
-      .then(data => {
-        refs.modalWindow.innerHTML = modalTemplate(data);
+    if (e.target.nodeName === 'IMG' || e.target.nodeName === 'SPAN' || e.target.nodeName === 'H2' || e.target.nodeName === 'P' || e.target.nodeName === 'LI') {
+        api
+            .fetchModalDetails(eventSingleCard.id, eventSingleCard.dataset.type)
+            .then(data => {
+                refs.modalWindow.innerHTML = modalTemplate(data);
 
-        onCLickBtnClose();
+                onCLickBtnClose();
 
-        const modalTitleRef = document.querySelector('.modal__text');
-        modalTitleRef.textContent = `${modalTitleRef.textContent.slice(0, 150)}...`;
-        // console.log(modalTitleRef.textContent);
+                const modalTitleRef = document.querySelector('.modal__text');
+                modalTitleRef.textContent = `${modalTitleRef.textContent.slice(0, 150)}...`;
+                // console.log(modalTitleRef.textContent);
 
-        const modalButtonMore = document.querySelector('.modal__btn__more');
-        modalButtonMore.addEventListener('click', onModalButtonMoreClick);
-        // console.log(data.name);
+                const modalButtonMore = document.querySelector('.modal__btn__more');
+                modalButtonMore.addEventListener('click', onModalButtonMoreClick);
+                // console.log(data.name);
 
-        const imageElement = document.querySelector('.modal-img-test');
+                const imageElement = document.querySelector('.modal-img-test');
 
-        // let lagestImage = data.images[0];
+                // let lagestImage = data.images[0];
 
-        const biggestImage = filterBiggerImage(data.images);
-        if (biggestImage) {
-          imageElement.setAttribute('src', biggestImage.url);
-        } else {
-          imageElement.setAttribute(
-            'src',
-            'https://image.flaticon.com/icons/png/512/4076/4076525.png',
-          );
-        }
+                const biggestImage = filterBiggerImage(data.images);
+                if (biggestImage) {
+                    imageElement.setAttribute('src', biggestImage.url);
+                } else {
+                    imageElement.setAttribute(
+                        'src',
+                        'https://image.flaticon.com/icons/png/512/4076/4076525.png',
+                    );
+                }
 
-        const imageElementCircle = document.querySelector('.modal__circle-img');
-        if (biggestImage) {
-          imageElementCircle.setAttribute('src', biggestImage.url);
-        } else {
-          imageElementCircle.setAttribute(
-            'src',
-            'https://image.flaticon.com/icons/png/512/4076/4076525.png',
-          );
-        }
-      })
-      .catch(error => console.log(error));
-  }
+                const imageElementCircle = document.querySelector('.modal__circle-img');
+                if (biggestImage) {
+                    imageElementCircle.setAttribute('src', biggestImage.url);
+                } else {
+                    imageElementCircle.setAttribute(
+                        'src',
+                        'https://image.flaticon.com/icons/png/512/4076/4076525.png',
+                    );
+                }
+            })
+            .catch(error => console.log(error));
+    }
 }
