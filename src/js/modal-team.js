024df9ad7398a1170fe8cbf456556b2434ab0d31
LidiaStrelets <0630610171li@gmail.com>
2021-06-22 22:41:45 +0300

    team modal small fix

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -1,14 +1,21 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('.team__backdrop'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  refs.backdrop.addEventListener('click', event => {
+    if (event.target === event.currentTarget) {
+      toggleModal();
     }
-  })();
+  });
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
