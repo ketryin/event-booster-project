10e7220205d966286671f0b307d8081183f9710e
Evgeniy Yurchak <zhenyaa2007@gmail.com>
2021-06-23 12:31:45 +0300

    adaptive-style

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -3,7 +3,7 @@
       openModalBtn: document.querySelector('[data-modal-open]'),
       closeModalBtn: document.querySelector('[data-modal-close]'),
       modal: document.querySelector('[data-modal]'),
-    };
+     };
   
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
@@ -11,4 +11,4 @@
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
     }
-  })();
+     })();
